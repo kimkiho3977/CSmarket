73bf4e4cc2556f121c7dc058db6d90c68dd058c0
kimkiho3977 <62974690+kimkiho3977@users.noreply.github.com>
2024-07-31 10:48:49 +0900

    empty project

---
+++ csmarket/src/main/java/spring_csmarket/csmarket/CsmarketApplication.java
@@ -0,0 +1,13 @@
+package spring_csmarket.csmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmarketApplication.class, args);
+	}
+
+}
---
+++ csmarket/src/test/java/spring_csmarket/csmarket/CsmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_csmarket.csmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
